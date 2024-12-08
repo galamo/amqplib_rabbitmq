f6bbf1c45389d2700192df33e38a6d25a6938ae1
galamo <galamouyal88@gmail.com>
2024-12-08 20:14:25 +0200

    a

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Rmq connection
+1. `cd compose`
+2. run `docker compose up` inside the compose folder
+3. run `npm install` inside the root folder
+4. run `node sender.js` & `node receiver.js`
--- sender.js
+++ sender.js
@@ -3,33 +3,33 @@
 const amqp = require("amqplib/callback_api");
 
 amqp.connect("amqp://localhost", function (error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function (error1, channel) {
-    if (error1) {
-      throw error1;
+    if (error0) {
+        throw error0;
     }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
 
-    const queue = "email_message";
+        const queue = "email_message";
 
-    channel.assertQueue(queue, {
-      durable: false,
+        channel.assertQueue(queue, {
+            durable: false,
+        });
+        setInterval(() => {
+            const msg = getMessage();
+            channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)));
+            console.log(`[Producer][Sent] => ${msg.messageId} send to queue: ${queue}`);
+        }, 300);
     });
-    setInterval(() => {
-      const msg = getMessage();
-      channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)));
-      console.log(`[Producer][Sent] => ${msg} send to queue: ${queue}`);
-    }, 2000);
-  });
 });
 let inc = 0;
 function getMessage() {
-  return {
-    messageId: inc++,
-    payload: "Some message payload",
-    timestamp: new Date().toISOString(),
-  };
+    return {
+        messageId: inc++,
+        payload: "Some message payload",
+        timestamp: new Date().toISOString(),
+    };
 }
 
 // {
